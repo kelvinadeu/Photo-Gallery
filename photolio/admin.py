f8c7d54ec7fe7342858ca789c9ebb7120bb5dfc6
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-01 16:19:38 +0300

    add to admin

--- photolio/admin.py
+++ photolio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cartegory,Location,Images
 
 # Register your models here.
+admin.site.register(Cartegory)
+admin.site.register(Location)
+admin.site.register(Images)
