ed5e5abf0d46dbf30474212de470c5f5d7eee438
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-01 18:48:18 +0300

    add to index.html

--- photolio/views.py
+++ photolio/views.py
@@ -4,8 +4,8 @@ from .models import Location,Cartegory,Images
 
 # Create your views here.
 def index(request):
-    Images=images.object.all(null=True)
-    Location=location.object.all(null=True)
+    images=Images.objects.all()
+    location=Location.objects.all()
     print(Images)
     return render(request,'index.html',{'images':images,'location':location})
 # Create your views here.
