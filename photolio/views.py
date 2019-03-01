608514c1781703654e13518e7099a8115ef58af2
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-01 18:03:44 +0300

    add to views.py

--- photolio/views.py
+++ photolio/views.py
@@ -4,7 +4,7 @@ from .models import Location,Cartegory,Images
 
 # Create your views here.
 def index(request):
-    Images=image.object.all(null=True)
+    Images=images.object.all(null=True)
     Location=location.object.all(null=True)
     print(Images)
     return render(request,'index.html',{'images':images,'location':location})
@@ -14,7 +14,7 @@ def search_cartegory(request):
 
     if 'cartegory' in request.GET and request.GET["cartegory"]:
         search_term = request.GET.get("cartegory")
-        searched_images = Images.search_cartegory(search_term)
+        searched_images = Images.search_by_cartegory(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"images": searched_images,"location":location})
