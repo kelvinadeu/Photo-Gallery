f44c414702b0454e936a704f861b05ffd4f2d973
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-05 13:04:33 +0300

    deployment to heroku

--- Galore/settings.py
+++ Galore/settings.py
@@ -52,12 +52,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dx2o3a=3&#=97cajej$r7(ha4p=j_+yy_6e%!capes1sj$g(ij'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
